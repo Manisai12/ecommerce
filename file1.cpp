a6309b67a3de6ed9165ab3008ae1f415798c9f4f
Manisai12 <manisaikukkadapu@gmail.com>
2025-08-26 06:03:29 +0530

    chatbox UI

---
+++ file1.cpp
@@ -0,0 +1,24 @@
+ // Java program to find maximum product of a subset.
+import java.util.*;
+class MinProductSubset
+{
+	static int minProductSubset(int a[], int n)
+	{
+    	 for(int i=0;i<n-1;i++)	{
+            
+    	 
+    	 }
+
+	// main function
+	public static void main(String[] args)
+	{
+
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+        int a[]=new int[n];
+		for(int i=0;i<n;i++)
+			a[i]=sc.nextInt();
+		System.out.println(minProductSubset(a, n));
+}}
+
+
